850dd34ed3384d0a0b7dbd047689fe9af907a82d
Leon Bohmann <leon.bohmann@mepla.net>
2023-08-06 23:41:08 +0200

    make main module working

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Fracture Suite
 
 This package helps identifying splinters on broken glass plys.
+
+## Installation
+
+```bat
+pip install fracsuite
+```
+
+## Usage
+```bat
+py -m fracsuite -image "path/to/image" [--crop]
+```
--- fracsuite/__main__.py
+++ fracsuite/__main__.py
@@ -9,9 +9,6 @@ parser.add_argument('--crop', action='store_true', \
     help='Instruct the analyzer to crop the input image.')
 args = parser.parse_args()    
 
-args.image = r"d:\Forschung\Glasbruch\Versuche.Reihe\Proben\4.70.22.A\fracture\LS\2923 2023-06-15 17h11 pre (0,308-4323,4963) [Transmission].bmp"
-args.crop = True
-
 print(args)
 
 analyzer = Analyzer(args.image, args.crop)
