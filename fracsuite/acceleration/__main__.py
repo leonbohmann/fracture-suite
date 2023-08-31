b4ec3cd6e620496ed11944920486254e9a3363b4
Leon Bohmann <leon.bohmann@mepla.net>
2023-08-31 22:42:29 +0200

    move test suite to test2

--- fracsuite/acceleration/__main__.py
+++ fracsuite/acceleration/__main__.py
@@ -69,7 +69,118 @@ def test3(reader: APReader):
     fig.savefig(os.path.join(out_dir, f"{reader.fileName}_detail.png"))
 
 
-def test2(reader: APReader, time: Channel, impact_time_id):
+def test2(reader: APReader):
+    channel_max = [(x.Name, np.max(x.data), np.min(x.data)) for x in reader.Channels]
+
+    for max in channel_max:
+        print(f'{max[0]}: {max[1]} / {max[2]}')
+
+    # channels: 0: Piezoelectric, 1: Piezoresistive
+    channels = [x for x in reader.Channels if x.Name.startswith("Fall")]
+
+    g = 9.81 # m/s²
+    drop_acc = channels[1] 
+    drop_acc1 = channels[0].data
+    time = drop_acc.Time.data
+
+    # this shifts the acc signal to zero before the impact
+    drop_avg = np.average(drop_acc.data[-120000:])
+    drop_data = drop_acc.data - drop_avg
+    drop_data = drop_data * g
+
+    # estimate savgol filter parameters based on the data time interval
+    time_interval = time[1] - time[0]
+    window_length = int(0.1 / time_interval)
+    if window_length % 2 == 0:
+        window_length += 1
+    polyorder = 3
+
+
+    # savgol
+    drop_data_smooth = signal.savgol_filter(drop_data, 140, 5)
+
+    # firwin
+    cutoff_frequency = 0.1  # Adjust as needed
+    num_taps = 101  # Filter length, adjust as needed
+    fir_filter = signal.firwin(num_taps, cutoff_frequency)
+    # drop_data_smooth = signal.lfilter(fir_filter, 1.0, drop_data)
+    # np.savetxt("output.txt", np.column_stack([time, drop_data]), fmt = ['%.8f', '%.8f'])
+    # drop_data = drop_data_smooth
+
+    a = drop_data.copy()
+
+    impact_w = 10
+    impact_time_id = np.argmax(np.abs(a))
+    impact_end_index = impact_time_id + impact_w//2
+    impact_start_index = impact_time_id - impact_w//2
+
+    # smooth g around the impact
+    a[:impact_start_index] = drop_data_smooth[:impact_start_index]
+    a[impact_end_index:] = drop_data_smooth[impact_end_index:]
+
+
+    # shift a to zero around the impact
+    impact_time = time[impact_time_id]
+    impact_start_index = time_to_index(time, impact_time - 0.5)
+    impact_end_index = time_to_index(time, impact_time + 1.5)
+
+
+    # take aaverage around impact_start and end index
+
+    # shift g into zero
+    g_shift = np.average(a[:impact_start_index])
+    g_shift2 = np.average(a[impact_end_index:])
+    g_shift =( g_shift + g_shift2) / 2
+    a = a - g_shift
+
+
+    # initial is needed, so that the length of the result is the same as the input
+    v = integrate.cumulative_trapezoid(a, time, initial = 0)
+    s = integrate.cumulative_trapezoid(v, time, initial = 0)
+
+    # shift the drop_data to zero after the impact
+    # find the impact time index
+    # impact_time_id = np.argmax(np.abs(v))
+    # impact_time = time[impact_time_id]
+    # impact_end_index = time_to_index(time, impact_time + 1.5)
+
+    # g_shift = np.average(drop_data_smooth[impact_end_index:])
+    # drop_data_s = drop_data_smooth.copy()
+    # # shift data from impact to end
+    # drop_data_s[impact_time_id+1:] = drop_data_s[impact_time_id+1:] - g_shift
+
+    # # calculate g shift after impact
+    # v_shift = np.average(v[impact_end_index:])
+    # v_shifted = v.copy()
+    # # shift data from impact to end
+    # v_shifted[impact_time_id+1:] = v_shifted[impact_time_id+1:] - v_shift
+
+    # use the corrected g now
+    # v = integrate.cumulative_trapezoid(drop_data_s, time, initial = 0)
+
+    # s_shifted = integrate.cumulative_trapezoid(v_shifted, time, initial = 0)
+    # s = integrate.cumulative_trapezoid(v, time, initial = 0)
+
+    # fig0 = plt.plot()
+
+    # plt.plot(time, drop_data, "gray", label="Acceleration")
+    # plt.plot(time, drop_data_smooth, "b", label="Smoothed")
+    # plt.plot(time, drop_data_s, "g", label="Adjusted")
+
+    # plt.legend()
+    # plt.show()
+
+    fig,axs = plot_multiple_datasets([ \
+        (time, drop_acc1, "k--", "Acc 1 [g]", "Ac1"), \
+        (time, a, "m-", "Acc [m/s²]", "Acceleration"),
+        (time, drop_data, "r--", "Acc [m/s²]", "Acceleration"),
+        # (time, v_shifted, "b--", "Speed [m/s]", "Acceleration"), 
+        (time, v, "b", "Speed [m/s]", "Speed"),
+        # (time, s_shifted, "g--", "Distance [m]", "Distance"),
+        (time, s, "g", "Distance [m]", "Distance")], 
+        'Time integrals of Acceleration')
+    fig.savefig(os.path.join(out_dir, "fall.png"))
+    
     time = time - time[impact_time_id-5]
     print([x.Name for x in reader.Channels])
     # Auswertung auf Impact beziehen und Zeit ab dort messen
@@ -134,114 +245,5 @@ if args.test is not None:
 
 # slow_group.plot()
 
-channel_max = [(x.Name, np.max(x.data), np.min(x.data)) for x in reader.Channels]
 
-for max in channel_max:
-    print(f'{max[0]}: {max[1]} / {max[2]}')
-
-# channels: 0: Piezoelectric, 1: Piezoresistive
-channels = [x for x in reader.Channels if x.Name.startswith("Fall")]
-
-g = 9.81 # m/s²
-drop_acc = channels[1] 
-drop_acc1 = channels[0].data
-time = drop_acc.Time.data
-
-# this shifts the acc signal to zero before the impact
-drop_avg = np.average(drop_acc.data[-120000:])
-drop_data = drop_acc.data - drop_avg
-drop_data = drop_data * g
-
-# estimate savgol filter parameters based on the data time interval
-time_interval = time[1] - time[0]
-window_length = int(0.1 / time_interval)
-if window_length % 2 == 0:
-    window_length += 1
-polyorder = 3
-
-
-# savgol
-drop_data_smooth = signal.savgol_filter(drop_data, 140, 5)
-
-# firwin
-cutoff_frequency = 0.1  # Adjust as needed
-num_taps = 101  # Filter length, adjust as needed
-fir_filter = signal.firwin(num_taps, cutoff_frequency)
-# drop_data_smooth = signal.lfilter(fir_filter, 1.0, drop_data)
-# np.savetxt("output.txt", np.column_stack([time, drop_data]), fmt = ['%.8f', '%.8f'])
-# drop_data = drop_data_smooth
-
-a = drop_data.copy()
-
-impact_w = 10
-impact_time_id = np.argmax(np.abs(a))
-impact_end_index = impact_time_id + impact_w//2
-impact_start_index = impact_time_id - impact_w//2
-
-# smooth g around the impact
-a[:impact_start_index] = drop_data_smooth[:impact_start_index]
-a[impact_end_index:] = drop_data_smooth[impact_end_index:]
-
-
-# shift a to zero around the impact
-impact_time = time[impact_time_id]
-impact_start_index = time_to_index(time, impact_time - 0.5)
-impact_end_index = time_to_index(time, impact_time + 1.5)
-
-
-# take aaverage around impact_start and end index
-
-# shift g into zero
-g_shift = np.average(a[:impact_start_index])
-g_shift2 = np.average(a[impact_end_index:])
-g_shift =( g_shift + g_shift2) / 2
-a = a - g_shift
-
-
-# initial is needed, so that the length of the result is the same as the input
-v = integrate.cumulative_trapezoid(a, time, initial = 0)
-s = integrate.cumulative_trapezoid(v, time, initial = 0)
-
-# shift the drop_data to zero after the impact
-# find the impact time index
-# impact_time_id = np.argmax(np.abs(v))
-# impact_time = time[impact_time_id]
-# impact_end_index = time_to_index(time, impact_time + 1.5)
-
-# g_shift = np.average(drop_data_smooth[impact_end_index:])
-# drop_data_s = drop_data_smooth.copy()
-# # shift data from impact to end
-# drop_data_s[impact_time_id+1:] = drop_data_s[impact_time_id+1:] - g_shift
-
-# # calculate g shift after impact
-# v_shift = np.average(v[impact_end_index:])
-# v_shifted = v.copy()
-# # shift data from impact to end
-# v_shifted[impact_time_id+1:] = v_shifted[impact_time_id+1:] - v_shift
-
-# use the corrected g now
-# v = integrate.cumulative_trapezoid(drop_data_s, time, initial = 0)
-
-# s_shifted = integrate.cumulative_trapezoid(v_shifted, time, initial = 0)
-# s = integrate.cumulative_trapezoid(v, time, initial = 0)
-
-# fig0 = plt.plot()
-
-# plt.plot(time, drop_data, "gray", label="Acceleration")
-# plt.plot(time, drop_data_smooth, "b", label="Smoothed")
-# plt.plot(time, drop_data_s, "g", label="Adjusted")
-
-# plt.legend()
-# plt.show()
-
-fig,axs = plot_multiple_datasets([ \
-    (time, drop_acc1, "k--", "Acc 1 [g]", "Ac1"), \
-    (time, a, "m-", "Acc [m/s²]", "Acceleration"),
-    (time, drop_data, "r--", "Acc [m/s²]", "Acceleration"),
-    # (time, v_shifted, "b--", "Speed [m/s]", "Acceleration"), 
-    (time, v, "b", "Speed [m/s]", "Speed"),
-    # (time, s_shifted, "g--", "Distance [m]", "Distance"),
-    (time, s, "g", "Distance [m]", "Distance")], 
-    'Time integrals of Acceleration')
-fig.savefig(os.path.join(out_dir, "fall.png"))
 
