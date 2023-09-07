a9c964f82005ad97189ef084684adce33b9d308a
Leon Bohmann <leon.bohmann@mepla.net>
2023-09-07 10:35:26 +0200

    clarifications in scalper script

--- fracsuite/scalper/__main__.py
+++ fracsuite/scalper/__main__.py
@@ -409,86 +409,90 @@ def get_specimens_from_projects(projects) -> list[Specimen]:
     
     return specimens
 
-if __name__ == "__main__":
+########################################################################################
+#   SCRIPT AREA
+#
+#
 
-    # implement parse to make this script callable from outside
-    parser = argparse.ArgumentParser(description=descr, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-project', nargs="?", help='The project to be processed.', default=None)  
-    parser.add_argument('-output', nargs="?", help='Output directory.')  
-    parser.add_argument('-folder', nargs="?", help='If this is passed, analyzes all scalp projects in a folder.', default=None)  
-    parser.add_argument('--mohr', action='store_true', help='Display mohr"sche circle for stress states.')
-    args = parser.parse_args()
-    
-    # overwrite global vars
-    SWITCH_DISPLAY_MOHR_CIRCLE = args.mohr
-    
-    # input vars
-    project_file = args.project
-    output_directory = args.output
-    
-    output_extension = "txt"
-    open_output_directory = True
-    
-    # default project file
-    if project_file is None and args.folder is None:
-        raise Exception("No project file or folder supplied!")
-    # default project file
-    if output_directory is None:
-        if project_file is not None:
-            output_directory = os.path.join(os.path.dirname(project_file), "out")
-        elif args.folder is not None:
-            output_directory = os.path.join(os.path.dirname(args.folder), "out")
-    
-    # create the output folder if it does not exist
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-    
-    projects: list[ScalpProject] = []
-    
-    # analyze all projects in the folder
-    if args.folder is not None:
-        project_folder = args.folder
-        extension = '.scp'
-        files = glob.glob(os.path.join(project_folder, '**', f'*{extension}'), recursive=True)
-        
-        for file in files:
-            print(f'Analyze file: {file.replace(project_folder, "")}')
-            project = ScalpProject(file)
-            project.write_measurements(output_directory, output_extension)
-            projects.append(project)
-    # analyze one project
-    else:
-        project = ScalpProject(project_file)
-        project.write_measurements(output_directory, output_extension)    
 
+# implement parse to make this script callable from outside
+parser = argparse.ArgumentParser(description=descr, formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-project', nargs="?", help='The project to be processed.', default=None)  
+parser.add_argument('-output', nargs="?", help='Output directory.')  
+parser.add_argument('-folder', nargs="?", help='If this is passed, analyzes all scalp projects in a folder.', default=None)  
+parser.add_argument('--mohr', action='store_true', help='Display mohr"sche circle for stress states.')
+args = parser.parse_args()
+
+# overwrite global vars
+SWITCH_DISPLAY_MOHR_CIRCLE = args.mohr
+
+# input vars
+project_file = args.project
+output_directory = args.output
+
+output_extension = "txt"
+open_output_directory = True
+
+# default project file
+if project_file is None and args.folder is None:
+    raise Exception("No project file or folder supplied!")
+# default project file
+if output_directory is None:
+    if project_file is not None:
+        output_directory = os.path.join(os.path.dirname(project_file), "out")
+    elif args.folder is not None:
+        output_directory = os.path.join(os.path.dirname(args.folder), "out")
+
+# create the output folder if it does not exist
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+projects: list[ScalpProject] = []
+
+# analyze all projects in the folder
+if args.folder is not None:
+    project_folder = args.folder
+    extension = '.scp'
+    files = glob.glob(os.path.join(project_folder, '**', f'*{extension}'), recursive=True)
+    
+    for file in files:
+        print(f'Analyze file: {file.replace(project_folder, "")}')
+        project = ScalpProject(file)
+        project.write_measurements(output_directory, output_extension)
         projects.append(project)
-        
-        
-    # write a summary    
-    project_len = len(projects)
-    measurements_len = np.sum([len(x.measurements) for x in projects])
-    
-    print(f'Projects       : {project_len}')
-    print(f'Measurements   : {measurements_len}')
-    
-    # output all specimens independently from their project    
-    specimens = get_specimens_from_projects(projects)
-    for specimen in specimens:
-        
-        pre = ""
-        if not all(not t.invalid for t in specimen.measurementlocations):
-            pre = "!!!"
-            
-        print(f'\t{pre}\t{specimen.name:10}: {len(specimen.measurementlocations)} Locations ({[(x.location_name, [l.orientation for l in x.measurements]) for x in specimen.measurementlocations]})')
-        
-      
-    # calculate stresses for specimens
-    for specimen in specimens:
+# analyze one project
+else:
+    project = ScalpProject(project_file)
+    project.write_measurements(output_directory, output_extension)    
 
-        print(f'\t{specimen.name:10}: sig_h={specimen.sig_h:.2f} (+- {specimen.sig_h_dev:.2f})MPa')
-        print('\t\t', end="")
+    projects.append(project)
+    
+    
+# write a summary    
+project_len = len(projects)
+measurements_len = np.sum([len(x.measurements) for x in projects])
+
+print(f'Projects       : {project_len}')
+print(f'Measurements   : {measurements_len}')
+
+# output all specimens independently from their project    
+specimens = get_specimens_from_projects(projects)
+for specimen in specimens:
+    
+    pre = ""
+    if not all(not t.invalid for t in specimen.measurementlocations):
+        pre = "!!!"
         
-        for loc in specimen.measurementlocations:
-            print(f'\t{loc.location_name}={loc.stress[0]:.2f}/{loc.stress[1]:.2f}', end = "")
-            
-        print()
+    print(f'\t{pre}\t{specimen.name:10}: {len(specimen.measurementlocations)} Locations ({[(x.location_name, [l.orientation for l in x.measurements]) for x in specimen.measurementlocations]})')
+    
+    
+# calculate stresses for specimens
+for specimen in specimens:
+
+    print(f'\t{specimen.name:10}: sig_h={specimen.sig_h:.2f} (+- {specimen.sig_h_dev:.2f})MPa')
+    print('\t\t', end="")
+    
+    for loc in specimen.measurementlocations:
+        print(f'\t{loc.location_name}={loc.stress[0]:.2f}/{loc.stress[1]:.2f}', end = "")
+        
+    print()
