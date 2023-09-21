1d0493a09bd08f905c2119b6e4a339ed61b2c489
Leon Bohmann <leon.bohmann@mepla.net>
2023-09-21 13:00:51 +0200

    create function to fill missing scalp data into uebersicht

--- fracsuite/tools/scalp.py
+++ fracsuite/tools/scalp.py
@@ -1,10 +1,13 @@
 import typer
 from rich import print
+from rich.progress import track
+from fracsuite.tools.general import GeneralSettings
 
 from fracsuite.tools.specimen import Specimen
 from fracsuite.scalper.scalpSpecimen import ScalpSpecimen
 
 scalp_app = typer.Typer()
+general = GeneralSettings.get()
 
 @scalp_app.command()
 def sync():
@@ -14,9 +17,48 @@ def sync():
         specimen.__scalp.calc()
         specimen.__scalp.save(specimen.scalp_dir)
 
-def fill(path: str):
+@scalp_app.command()
+def fill():
+    from openpyxl import load_workbook
+    from openpyxl.worksheet.worksheet import Worksheet
+
+    workbook_path = general.get_output_file("..", "Uebersicht.xlsx")
+    try:
+        workbook = load_workbook(workbook_path)
+    except ValueError:
+        print(f"Could not load workbook at {workbook_path}. Maybe the filter is still applied?")
+        return
+    db: Worksheet = workbook['Datenbank']
+
     specimens = Specimen.get_all()
 
-    for specimen in specimens:
-        specimen.__scalp.calc()
-        specimen.__scalp.save(specimen.scalp_dir)
+    sigmas = {
+        4: {}, 8: {}, 12: {}
+    }
+    for spec in specimens:
+        if spec.thickness not in sigmas:
+            sigmas[spec.thickness] = {}
+        if spec.nom_stress not in sigmas[spec.thickness]:
+            sigmas[spec.thickness][spec.nom_stress] = {}
+        if spec.boundary not in sigmas[spec.thickness][spec.nom_stress]:
+            sigmas[spec.thickness][spec.nom_stress][spec.boundary] = {}
+        if spec.nbr not in sigmas[spec.thickness][spec.nom_stress][spec.boundary]:
+            sigmas[spec.thickness][spec.nom_stress][spec.boundary][spec.nbr] = spec.sig_h
+
+
+    row = 2
+    for i in track(range(row, 800)):
+        try:
+            t = int(db[f'A{row}'].value)
+            s = int(db[f'B{row}'].value)
+            l = str(db[f'C{row}'].value)
+            i = int(db[f'D{row}'].value)
+
+            db[f'H{row}'].value = sigmas[t][s][l][i]
+        except:
+            pass
+
+        row += 1
+
+    workbook.save(workbook_path)
+    workbook.close()
