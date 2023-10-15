fixed moving average plots

fracsuite/core/kernels.py
@@ -3,6 +3,7 @@ import numpy as np
 from fracsuite.core.image import split_image
 from fracsuite.state import State
 from rich.progress import track
+from rich import print
 from fracsuite.general import GeneralSettings
 
 class ImageKerneler():
@@ -89,14 +90,14 @@ class ObjectKerneler():
         region: tuple[int, int],
         data_objects: list[T],
         collector: Callable[[T, tuple[int,int,int,int]], bool],
-        kernel_width: float = 200,
+        kw_px: int = 200,
         skip_edge: bool = False,
         skip_edge_factor: float = 0.02,
     ):
         self.region = region
         self.data_objects = data_objects
         self.collector = collector
-        self.kernel_width = kernel_width
+        self.kw_px = kw_px
         self.skip_edge = skip_edge
         self.edgeskip_factor = skip_edge_factor
 
@@ -121,11 +122,11 @@ class ObjectKerneler():
         calculator: Callable[[list[T]], float],
         n_points: int,
     ) -> np.ndarray:
-        assert self.kernel_width < self.region[0], \
+        assert self.kw_px < self.region[0], \
             "Kernel width must be smaller than the region width."
-        assert self.kernel_width < self.region[1], \
+        assert self.kw_px < self.region[1], \
             "Kernel width must be smaller than the region height."
-        assert self.kernel_width > 10, \
+        assert self.kw_px > 10, \
             "Kernel width must be greater than 10."
         assert len(self.data_objects) > 0, \
             "There must be at least one object in the list."
@@ -146,10 +147,10 @@ class ObjectKerneler():
                 if w != h:
                     continue
 
-                x1 = (w/n_points) * px_w - self.kernel_width // 2
-                y1 = (h/n_points) * px_h - self.kernel_width // 2
-                x2 = x1 + self.kernel_width
-                y2 = y1 + self.kernel_width
+                x1 = (w/n_points) * px_w - self.kw_px // 2
+                y1 = (h/n_points) * px_h - self.kw_px // 2
+                x2 = x1 + self.kw_px
+                y2 = y1 + self.kw_px
 
                 objects_in_region = \
                     [obj for obj in self.data_objects \
@@ -165,17 +166,25 @@ class ObjectKerneler():
         self,
         calculator: Callable[[list[T]], float],
     ):
+        print(f'[cyan]KERNELER[/cyan] Kernel Width: {self.kw_px}px')
+        print(f'[cyan]KERNELER[/cyan] Region      : {self.region}px')
         # Get the ranges for x and y
         minx = 0.0
         maxx = np.max(self.region[0])
         miny = 0.0
         maxy = np.max(self.region[1])
-
-        xd = np.linspace(minx, maxx, int(np.round(maxx/self.kernel_width)))
-        yd = np.linspace(miny, maxy, int(np.round(maxy/self.kernel_width)))
+        # xd = np.linspace(minx, maxx, int(np.round(maxx/self.kernel_width)))
+        # yd = np.linspace(miny, maxy, int(np.round(maxy/self.kernel_width)))
+        xd = np.linspace(minx, maxx, int(np.round(maxx/self.kw_px)))
+        yd = np.linspace(miny, maxy, int(np.round(maxy/self.kw_px)))
         X, Y = np.meshgrid(xd, yd)
 
+        # shift the calculated points into the center
+        X = X + self.kw_px // 2
+        Y = Y + self.kw_px // 2
+
         Z = np.zeros_like(X, dtype=np.float64)
+        print(f'[cyan]KERNELER[/cyan] "{len(X)}x{len(Y)}" Points to process.')
 
         skip_i = int(len(xd)*self.edgeskip_factor) if self.skip_edge else 0
 
@@ -187,8 +196,8 @@ class ObjectKerneler():
 
         for i in d:
             for j in range(skip_i,len(yd) - skip_i):
-                x1,y1=xd[i]-self.kernel_width//2,yd[j]-self.kernel_width//2
-                x2,y2=xd[i]+self.kernel_width//2,yd[j]+self.kernel_width//2
+                x1,y1=xd[i]-self.kw_px//2,yd[j]-self.kw_px//2
+                x2,y2=xd[i]+self.kw_px//2,yd[j]+self.kw_px//2
 
                 # Create a region (x1, y1, x2, y2)
                 region_rect = (x1, y1, x2, y2)
@@ -201,4 +210,7 @@ class ObjectKerneler():
                 Z[j, i] = calculator(objects_in_region) \
                     if len(objects_in_region) > 0 else 0
 
+        # this is for testing
+        # Z[0,0] = 1000
+        # Z[-1,-1] = 1000
         return X,Y,Z

fracsuite/core/plotting.py
@@ -7,6 +7,8 @@ import tempfile
 from enum import Enum
 from typing import Any, Callable
 
+from rich import print
+
 import cv2
 from deprecated import deprecated
 import matplotlib as mpl
@@ -69,7 +71,7 @@ def get_fig_width(w: FigWidth, hf=None, dimf=1.1) -> float:
     """
     Calculates the figure width and height in inches based on the given width factor, height factor and dimension factor.
 
-    The dimension factor is used so that the figures are rendered slightly larger than the actual size,
+    The dimension factor dimf is used so that the figures are rendered slightly larger than the actual size,
     because in Latex the textwidth is not the same as the actual width of the environment.
 
     Args:
@@ -129,7 +131,7 @@ def to_img(fig):
 
 def plot_image_movavg(
     image: np.ndarray,
-    kernel_width: float,
+    kw_px: float,
     z_action: Callable[[list[Splinter]], float] = None,
     clr_label=None,
     plot_vertices: bool = False,
@@ -139,6 +141,7 @@ def plot_image_movavg(
     no_ticks = True,
     figwidth = FigWidth.ROW2,
     clr_format: str = None,
+    normalize: bool = True,
     crange: tuple[float,float] = None,
 ) -> tuple[StateOutput, Axes]:
     """Create an intensity plot of the fracture.
@@ -162,21 +165,24 @@ def plot_image_movavg(
     """
 
     # print(f'Creating intensity plot with region={region}...')
-    kernel = ImageKerneler(image, kernel_width, skip_edge=skip_edge)
+    kernel = ImageKerneler(image, kw_px, skip_edge=skip_edge)
     X, Y, Z = kernel.run(z_action, exclude_points=exclude_points)
-    Z = Z / np.max(Z)
+
+    if normalize:
+        Z = Z / np.max(Z)
 
 
-    return plot_kernel_results(image, clr_label, no_ticks, plot_vertices, mode, X, Y, Z, figwidth=figwidth, clr_format=clr_format, crange=crange)
+    return plot_kernel_results(image, clr_label, no_ticks, plot_vertices, mode, X, Y, Z, kw_px, figwidth=figwidth, clr_format=clr_format, crange=crange)
 
 def plot_splinter_movavg(
     original_image: np.ndarray,
     splinters: list[Splinter],
-    kernel_width: float,
+    kw_px: int,
     z_action: Callable[[list[Splinter]], float] = None,
     clr_label: str = None,
     no_ticks: bool = True,
     plot_vertices: bool = False,
+    skip_edge: bool = False,
     mode: KernelContourMode = KernelContourMode.CONTOURS,
     figwidth: FigWidth = FigWidth.ROW1,
     clr_format=None,
@@ -195,7 +201,7 @@ def plot_splinter_movavg(
         splinters : list[Splinter]
             A list of Splinter objects representing the regions of interest in the image.
         kernel_width : float
-            The width of the kernel to use in the operation.
+            The width of the kernel to use in the operation in px.
         z_action : Callable[[list[Splinter]], float], optional
             A function that takes a list of Splinter objects and returns a scalar value
             to use as the z-coordinate of the kernel results. If None, the default
@@ -225,8 +231,8 @@ def plot_splinter_movavg(
     """
     assert KernelContourMode.has_value(mode), f"Contour mode must be one of {KernelContourMode.values()}. plot_splinter_movavg."
     assert FigWidth.has_value(figwidth), f"FigWidth must be one of {FigWidth.values()}. plot_splinter_movavg."
-    assert kernel_width > 0, "kernel_width must be greater than 0."
-    assert kernel_width < np.min(original_image.shape[:2]), "kernel_width must be smaller than the image size."
+    assert kw_px > 0, "kernel_width must be greater than 0."
+    assert kw_px < np.min(original_image.shape[:2]), "kernel_width must be smaller than the image size."
     assert figwidth in general.width_factors, f"figwidth {figwidth} not found."
 
     region = np.array([original_image.shape[1], original_image.shape[0]])
@@ -235,9 +241,9 @@ def plot_splinter_movavg(
     kernel = ObjectKerneler(
         region,
         splinters,
-        lambda x,r: x.in_region_px(r),
-        kernel_width,
-        skip_edge=False,
+        collector=lambda x,r: x.in_region_px(r),
+        kw_px=kw_px,
+        skip_edge=skip_edge,
         skip_edge_factor=0.02,
     )
 
@@ -246,8 +252,7 @@ def plot_splinter_movavg(
     if normalize:
         Z = Z / np.max(Z)
 
-
-    return plot_kernel_results(original_image, clr_label, no_ticks, plot_vertices, mode, X, Y, Z, figwidth=figwidth, clr_format=clr_format, crange=crange)
+    return plot_kernel_results(original_image, clr_label, no_ticks, plot_vertices, mode, X, Y, Z, kw_px, figwidth=figwidth, clr_format=clr_format, crange=crange)
 
 def plot_kernel_results(
     original_image,
@@ -258,6 +263,7 @@ def plot_kernel_results(
     X,
     Y,
     results,
+    kw_px,
     figwidth: FigWidth,
     clr_format: str = None,
     crange: tuple[float,float] = None
@@ -283,19 +289,29 @@ def plot_kernel_results(
     """
     figsize = get_fig_width(figwidth)
     fig,axs = plt.subplots(figsize=figsize)
-    axs.imshow(original_image)
 
-    if plot_vertices:
-            axs.scatter(X, Y, marker='o', c='red')
+    def show_img():
+        axs.imshow(original_image, interpolation='bilinear')
+
+    def show_vertices():
+        if plot_vertices:
+            axs.scatter(X, Y, marker='x', c='white', s=3, linewidth=0.5)
 
     if mode == KernelContourMode.CONTOURS:
-        axim = axs.contourf(X, Y, results, cmap='turbo', alpha=CONTOUR_ALPHA)
+        axim = axs.contourf(X-kw_px/2, Y-kw_px/2, results, cmap='turbo', alpha=CONTOUR_ALPHA)
+        show_vertices()
+        show_img()
     elif mode == KernelContourMode.FILLED:
-        z_im = cv2.resize(results, (original_image.shape[1], original_image.shape[0]), interpolation=cv2.INTER_LINEAR)
+        show_vertices()
+        show_img()
+        # z_im = cv2.resize(results, (original_image.shape[1], original_image.shape[0]), interpolation=cv2.INTER_LINEAR_EXACT)
+        results = cv2.resize(results, (original_image.shape[1], original_image.shape[0]), interpolation=cv2.INTER_LINEAR)
         if crange is None:
-            axim = axs.imshow(z_im, cmap='turbo', alpha=CONTOUR_ALPHA)
+            axim = axs.imshow(results, cmap='turbo', alpha=CONTOUR_ALPHA)
         else:
-            axim = axs.imshow(z_im, cmap='turbo', alpha=CONTOUR_ALPHA, vmin=crange[0], vmax=crange[1])
+            axim = axs.imshow(results, cmap='turbo', alpha=CONTOUR_ALPHA, vmin=crange[0], vmax=crange[1])
+
+
 
 
     if clr_label is not None:
@@ -337,7 +353,7 @@ def plot_kernel_results(
 
 def renew_ticks_cb(cbar):
     manual_ticks = [cbar.vmin, cbar.vmin + (cbar.vmax - cbar.vmin) / 2, cbar.vmax]
-    print(manual_ticks)
+    # print(manual_ticks)
     cbar.ax.set_yticks(manual_ticks)
     labels = cbar.ax.get_yticklabels()
 

fracsuite/splinters.py
@@ -152,7 +152,7 @@ def roughness_f(specimen_name: Annotated[str, typer.Argument(help='Name of speci
 
     fig = plot_splinter_movavg(specimen.get_fracture_image(),
                                         splinters=specimen.splinters,
-                                        kernel_width=kernel_width,
+                                        kw_px=kernel_width,
                                         z_action=roughness_function,
                                         clr_label='Mean roughness',
                                         fig_title='Splinter Roughness')
@@ -160,8 +160,10 @@ def roughness_f(specimen_name: Annotated[str, typer.Argument(help='Name of speci
     State.output(fig, spec=specimen)
 
 @app.command()
-def roundness_f(specimen_name: Annotated[str, typer.Argument(help='Name of specimens to load')],
-                kernel_width: Annotated[int, typer.Option(help='Size of the region to calculate the roughness on.')] = 200,):
+def roundness_f(
+    specimen_name: Annotated[str, typer.Argument(help='Name of specimens to load')],
+    w_mm: Annotated[int, typer.Option(help='Size of the region to calculate the roughness on.')] = 50,
+):
     """Create a contour plot of the roundness on the specimen.
 
     Args:
@@ -174,14 +176,18 @@ def roundness_f(specimen_name: Annotated[str, typer.Argument(help='Name of speci
     specimen = Specimen.get(specimen_name)
     assert specimen is not None, "Specimen not found."
 
-    fig = plot_splinter_movavg(specimen.get_fracture_image(),
-                                        splinters=specimen.splinters,
-                                        kernel_width=kernel_width,
-                                        z_action=roundness_function,
-                                        clr_label='Mean roughness',
-                                        fig_title='Splinter Roughness')
+    fig = plot_splinter_movavg(
+        specimen.get_fracture_image(),
+        splinters=specimen.splinters,
+        kw_px=w_mm / specimen.get_size_factor(),
+        z_action=roundness_function,
+        clr_label='Mean roughness',
+        mode=KernelContourMode.FILLED,
+        figwidth=FigWidth.ROW2,
+        clr_format=".1f",
+    )
 
-    State.output(fig, spec=specimen)
+    State.output(fig, spec=specimen, to_additional=True)
 
 @app.command()
 def roughness(specimen_name: Annotated[str, typer.Argument(help='Name of specimens to load')]):
@@ -375,7 +381,7 @@ def diag_dist_specimen_intensity_func(
         img.shape[:2],
         specimen.splinters,
         lambda x,r: x.in_region_px(r),
-        kernel_width=kernel_width,
+        kw_px=kernel_width,
         skip_edge=True,
     )
 
@@ -392,7 +398,7 @@ def log2dhist_diag(
     sigmas: Annotated[str, typer.Option(help='Stress range. Either a single value or a range separated by a dash (i.e. "100-110" or "120" or "all").', metavar='s, s1-s2, all')] = None,
     delta: Annotated[float, typer.Option(help='Additional range for sigmas.')] = 10,
     out: Annotated[str, typer.Option(help='Output file.')] = None,
-    kernel_width: Annotated[int, typer.Option(help='Intensity kernel width.')] = 200,
+    w_mm: Annotated[int, typer.Option(help='Intensity kernel width.')] = 50,
     n_points: Annotated[int, typer.Option(help='Amount of points on the diagonal to evaluate.')] = 100,
     y_stress: Annotated[bool, typer.Option(help='Plot sigma instead of energy on y-axis.')] = False,
 ):
@@ -421,11 +427,15 @@ def log2dhist_diag(
     with get_progress() as progress:
         an_task = progress.add_task("Loading splinters...", total=len(specimens))
 
+        sf = specimens[0].get_size_factor()
+        assert np.all([x.get_size_factor() == sf for x in specimens]), "Not all specimens have the same size factor."
+
+        w_px = w_mm / sf
 
 
         for intensity, specimen in pooled(specimens, diag_dist_specimen_intensity_func,
                                           advance = lambda: progress.advance(an_task),
-                                          kernel_width=kernel_width,
+                                          kernel_width=w_px,
                                           n_points=n_points):
             intensity = intensity / np.max(intensity)
 
@@ -716,7 +726,7 @@ def plot_histograms(xlim: tuple[float,float],
 @app.command()
 def splinter_orientation_f(
     specimen_name: Annotated[str, typer.Argument(help='Name of specimen to load')],
-    w_mm: Annotated[float, typer.Option(help='Width of kernel in mm.')] = 20,
+    w_mm: Annotated[float, typer.Option(help='Width of kernel in mm.')] = 50,
 ):
     specimen = Specimen.get(specimen_name)
     impact_pos = specimen.get_impact_position()
@@ -730,7 +740,7 @@ def splinter_orientation_f(
     fig_output = plot_splinter_movavg(
         specimen.get_fracture_image(),
         specimen.splinters,
-        kernel_width=w_px,
+        kw_px=w_px,
         z_action=mean_orientations,
         clr_label="Mean Orientation Strength $\Delta$",
         mode=KernelContourMode.FILLED,
@@ -788,7 +798,7 @@ def splinter_orientation(specimen_name: Annotated[str, typer.Argument(help='Name
 @app.command()
 def fracture_intensity_img(
     specimen_name: str,
-    w_mm: Annotated[int, typer.Option(help='Kernel width.')] = 20,
+    w_mm: Annotated[int, typer.Option(help='Kernel width.')] = 50,
     skip_edges: Annotated[bool, typer.Option(help='Skip 10% of the edges when calculating intensities.')] = True,
     figwidth: Annotated[FigWidth, typer.Option(help='Fraction of kernel width to use.')] = FigWidth.ROW2,
 ):
@@ -817,7 +827,7 @@ def fracture_intensity_img(
     w_px = int(w_mm / specimen.get_size_factor())
     output = plot_image_movavg(
         img,
-        kernel_width=w_px,
+        kw_px=w_px,
         z_action=mean_img_value,
         clr_label="Black Pixels [$N_{BP}/A/N_t$]",
         mode=KernelContourMode.FILLED,
@@ -832,8 +842,9 @@ def fracture_intensity_img(
 @app.command()
 def fracture_intensity_f(
         specimen_name: Annotated[str, typer.Argument(help='Name of specimen to load')],
-        w_mm: Annotated[int, typer.Option(help='Kernel width.')] = 20,
+        w_mm: Annotated[int, typer.Option(help='Kernel width.')] = 50,
         plot_vertices: Annotated[bool, typer.Option(help='Plot the kernel points.')] = False,
+        as_contours: Annotated[bool, typer.Option(help='Plot the kernel as contours.')] = False,
         skip_edges: Annotated[bool, typer.Option(help='Skip 10% of the edges when calculating intensities.')] = False,
         figwidth: Annotated[FigWidth, typer.Option(help='Fraction of kernel width to use.')] = FigWidth.ROW2,
     ):
@@ -853,13 +864,18 @@ def fracture_intensity_f(
         z_action=lambda x: len(x),
         plot_vertices=plot_vertices,
         clr_label="Fracture Intensity [$N_S/A$]", #, $w_A,h_A$={w_mm}mm
-        mode=KernelContourMode.FILLED,
+        mode=KernelContourMode.FILLED if not as_contours else KernelContourMode.CONTOURS,
         skip_edges=skip_edges,
         figwidth=figwidth,
-        clr_format='.0f'
+        clr_format='.0f',
     )
+    mods = []
+    if as_contours:
+        mods.append("contours")
+    if plot_vertices:
+        mods.append("vertices")
 
-    State.output(fig, spec=specimen, to_additional=True)
+    State.output(fig, spec=specimen, to_additional=True, mods=mods)
 
 @app.command()
 def create_voronoi(specimen_name: Annotated[str, typer.Argument(help='Name of specimen to load')],):
@@ -1219,7 +1235,8 @@ def compare_manual(
         State.output(cmp_alg_lab, folder, f'{nr}_compare_contours_watershed_label', to_additional=True)
         State.output_nopen(cmp_alg_leg, folder, f'{nr}_compare_contours_watershed_legacy')
 
-
+        # use this for better visibility
+        hist_bins = 20
         figwidth=FigWidth.ROW3
 
         fig,axs = datahist_plot(
@@ -1234,15 +1251,15 @@ def compare_manual(
         area1 = np.array([x.area for x in manual_splinters])
 
         if x_range is None:
-            _,br, d1 = datahist_to_ax(ax, area0, n_bins=30, label='Algorithm', color='red', plot_mean=False, as_density=False)
+            _,br, d1 = datahist_to_ax(ax, area0, n_bins=hist_bins, label='Algorithm', color='red', plot_mean=False, as_density=False)
         else:
             low, up = [float(x) for x in x_range.split(':')]
             low = low if low > 0 else 1
-            br = np.linspace(np.log10(low) if low > 0 else 1, np.log10(up), 20)
+            br = np.linspace(np.log10(low) if low > 0 else 1, np.log10(up), hist_bins)
             _,_, d1 = datahist_to_ax(ax, area0, binrange=br, label='Algorithm', color='red', plot_mean=False, as_density=False)
 
-        # datahist_to_ax(ax, [x.area for x in manual_splinters], n_bins=20, label='Manual', plot_mean=False)
-        # datahist_to_ax(ax, [x.area for x in legacy_splinters], n_bins=20, label='Legacy', plot_mean=False)
+        # datahist_to_ax(ax, [x.area for x in manual_splinters], n_bins=hist_bins, label='Manual', plot_mean=False)
+        # datahist_to_ax(ax, [x.area for x in legacy_splinters], n_bins=hist_bins, label='Legacy', plot_mean=False)
         _,_,d2 = datahist_to_ax(ax, area1, binrange=br, label='Manual', color='green', plot_mean=False, as_density=False)
         delta_area = np.abs(d1-d2)
         plt.bar(br[:-1], delta_area, width=np.diff(br), align="edge", alpha=1, label="Difference", color="blue")
@@ -1268,10 +1285,10 @@ def compare_manual(
         )
 
         if x_range is None:
-            binrange = np.linspace(np.min(area0), np.max(area0), 20)
+            binrange = np.linspace(np.min(area0), np.max(area0), hist_bins)
         else:
             low, up = [float(x) for x in x_range.split(':')]
-            binrange = np.linspace(low, up, 20)
+            binrange = np.linspace(low, up, hist_bins)
 
         sims=similarity(
             area0,