fixed moving average algorithm

fracsuite/core/imageprocessing.py
@@ -2,13 +2,34 @@ import cv2
 import numpy as np
 import numpy.typing as nptyp
 
-from fracsuite.core.image import is_rgb, to_gray, to_rgb
+from fracsuite.core.image import is_rgb, to_gray, to_rgb, to_rgba
 from fracsuite.core.imageplotting import plotImage
 from fracsuite.core.preps import PrepMode, PreprocessorConfig, defaultPrepConfig
 
 from rich import print
 
 W_FAC = 4000
+
+def make_transparent_border(image, border_percent=0.1, default_alpha=1.0):
+    height, width = image.shape[:2]
+
+    border_width = int(width *  border_percent)
+    border_height = int(height * border_percent)
+
+    mask = np.ones((height, width), dtype=np.uint8) * default_alpha
+
+    for i in range(border_height):
+        alpha_value = i* default_alpha / border_height
+        mask[i, :] = alpha_value
+        mask[-(i + 1), :] = alpha_value
+
+    for i in range(border_width):
+        alpha_value = i* default_alpha / border_width
+        mask[:, i] = np.minimum(mask[:, i], alpha_value)
+        mask[:, -(i + 1)] = np.minimum(mask[:, -(i + 1)], alpha_value)
+
+
+    return mask
 
 def lightcorrect(image, strength=5, size=8):
     # Convert to LAB color space

fracsuite/core/kernels.py
@@ -11,6 +11,20 @@ def print(*args, **kwargs):
     if State.debug:
         rich.print(*args, **kwargs)
 
+def convert_npoints(n_points, region, kw_px) -> tuple[int,int]:
+    if isinstance(n_points, tuple):
+        # Get the ranges for x and y
+        i_w = n_points[0]
+        i_h = n_points[1]
+    elif n_points == -1:
+        i_w = int(np.ceil(region[0]/kw_px))
+        i_h = int(np.ceil(region[1]/kw_px))
+    else:
+        i_w = n_points
+        i_h = n_points
+
+    return i_w, i_h
+
 class ImageKerneler():
     def __init__(
         self,
@@ -95,55 +109,72 @@ class ObjectKerneler():
         region: tuple[int, int],
         data_objects: list[T],
         collector: Callable[[T, RectRegion], bool],
-        kw_px: int = 200,
         skip_edge: bool = False,
         skip_edge_factor: float = 0.02,
     ):
         self.region = region
         self.data_objects = data_objects
         self.collector = collector
-        self.kw_px = kw_px
         self.skip_edge = skip_edge
         self.edgeskip_factor = skip_edge_factor
 
     def run(
         self,
         calculator: Callable[[list[T]], float],
-        n_points: int = 50,
+        kw_px: int,
+        n_points: int = -1,
         mode: str = "area",
         exclude_points: list[tuple[int,int]] = None,
     ) -> tuple[np.ndarray, np.ndarray, np.ndarray] | np.ndarray:
-        """Run the kerneler."""
+        """
+        Run the kerneler.
+
+        If n_points is -1, then the kerneler will use the kernel width to determine
+        the number of points to use.
+
+        For mode=='diag', the kerneler will only use the diagonal of the region and
+        return a 1D array with results.
+
+
+
+        Args:
+            calculator (Callable[[list[T]], float]): The calculator function to use.
+            kw_px (int): The kernel width in pixels. Defaults to 10.
+            n_points (int, optional): The number of points to use. Defaults to -1.
+            mode (str, optional): The mode to use. Defaults to "area".
+            exclude_points (list[tuple[int,int]], optional): A list of points to exclude. Defaults to None.
+
+        Returns:
+            tuple[np.ndarray, np.ndarray, np.ndarray] | np.ndarray: The result of the kerneler.
+        """
 
         assert mode in ObjectKerneler.__modes, \
             f"Mode must be one of '{ObjectKerneler.__modes}'."
 
         if mode == "area":
-            return self.__run_area(calculator, exclude_points=exclude_points)
+            return self.__run_area(calculator, n_points, kw_px, exclude_points=exclude_points)
         elif mode == "diag":
-            return self.__run_diag(calculator, n_points, exclude_points=exclude_points)
+            return self.__run_diag(calculator, n_points, kw_px, exclude_points=exclude_points)
 
     def __run_diag(
         self,
         calculator: Callable[[list[T]], float],
-        n_points: int,
+        n_points: int | tuple[int,int],
+        kw_px: int,
         exclude_points: list[tuple[int,int]] = None,
     ) -> np.ndarray:
-        assert self.kw_px < self.region[0], \
+        assert kw_px < self.region[0], \
             "Kernel width must be smaller than the region width."
-        assert self.kw_px < self.region[1], \
+        assert kw_px < self.region[1], \
             "Kernel width must be smaller than the region height."
-        assert self.kw_px > 10, \
+        assert kw_px > 10, \
             "Kernel width must be greater than 10."
         assert len(self.data_objects) > 0, \
             "There must be at least one object in the list."
-        assert n_points > 0, \
+        assert n_points > 0 or n_points == -1, \
             "n_points must be greater than 0."
 
-
-        # Get the ranges for x and y
-        i_w = n_points
-        i_h = n_points
+        i_w, i_h = convert_npoints(n_points, self.region, kw_px)
 
         px_w, px_h = self.region
 
@@ -154,10 +185,10 @@ class ObjectKerneler():
                 if w != h:
                     continue
 
-                x1 = (w/n_points) * px_w - self.kw_px // 2
-                y1 = (h/n_points) * px_h - self.kw_px // 2
-                x2 = x1 + self.kw_px
-                y2 = y1 + self.kw_px
+                x1 = (w/n_points) * px_w - kw_px // 2
+                y1 = (h/n_points) * px_h - kw_px // 2
+                x2 = x1 + kw_px
+                y2 = y1 + kw_px
 
                 objects_in_region = \
                     [obj for obj in self.data_objects \
@@ -172,26 +203,38 @@ class ObjectKerneler():
     def __run_area(
         self,
         calculator: Callable[[list[T]], float],
+        n_points: int | tuple[int,int],
+        kw_px: int,
         exclude_points: list[tuple[int,int]] = None,
     ):
-        print(f'[cyan]KERNELER[/cyan] Kernel Width: {self.kw_px}px')
+        assert n_points > 0 or n_points == -1, \
+            "n_points must be greater than 0."
+        assert kw_px < self.region[0], \
+            "Kernel width must be smaller than the region width."
+        assert kw_px < self.region[1], \
+            "Kernel width must be smaller than the region height."
+        assert kw_px > 10, \
+            "Kernel width must be greater than 10."
+        assert len(self.data_objects) > 0, \
+            "There must be at least one object in the list."
+
+        print(f'[cyan]KERNELER[/cyan] Kernel Width: {kw_px}px')
         print(f'[cyan]KERNELER[/cyan] Region      : {self.region}px')
 
         # Get the ranges for x and y
-        minx = 0.0
-        maxx = self.region[0]
-        miny = 0.0
-        maxy = self.region[1]
+        minx = kw_px // 2
+        maxx = self.region[0] - kw_px // 2
+        miny = kw_px // 2
+        maxy = self.region[1] - kw_px // 2
+
+        i_w, i_h = convert_npoints(n_points, self.region, kw_px)
 
         # xd = np.linspace(minx, maxx, int(np.round(maxx/self.kernel_width)))
         # yd = np.linspace(miny, maxy, int(np.round(maxy/self.kernel_width)))
-        xd = np.linspace(minx, maxx, int(np.ceil(maxx/self.kw_px)), endpoint=False)
-        yd = np.linspace(miny, maxy, int(np.ceil(maxy/self.kw_px)), endpoint=False)
+        xd = np.linspace(minx, maxx, i_w, endpoint=True)
+        yd = np.linspace(miny, maxy, i_h, endpoint=True)
         X, Y = np.meshgrid(xd, yd)
 
-        # shift the calculated points into the center
-        X = X + self.kw_px // 2
-        Y = Y + self.kw_px // 2
 
         Z = np.zeros_like(X, dtype=np.float64)
         print(f'[cyan]KERNELER[/cyan] "{len(xd)}x{len(xd)}" Points to process.')
@@ -206,8 +249,8 @@ class ObjectKerneler():
 
         for i in d:
             for j in range(skip_i,len(yd) - skip_i):
-                x1,x2=(xd[i],xd[i]+self.kw_px)
-                y1,y2=(yd[j],yd[j]+self.kw_px)
+                x1,x2=(xd[i]-kw_px//2,xd[i]+kw_px//2)
+                y1,y2=(yd[j]-kw_px//2,yd[j]+kw_px//2)
 
                 # Create a region (x1, y1, x2, y2)
                 region = RectRegion(x1, y1, x2, y2)

fracsuite/core/plotting.py
@@ -21,6 +21,7 @@ from matplotlib.ticker import FuncFormatter
 
 from fracsuite.core.coloring import get_color, norm_color, rand_col
 from fracsuite.core.image import to_rgb
+from fracsuite.core.imageprocessing import make_transparent_border
 from fracsuite.core.kernels import ImageKerneler, ObjectKerneler
 from fracsuite.core.splinter import Splinter
 from fracsuite.general import GeneralSettings
@@ -178,10 +179,12 @@ def plot_splinter_movavg(
     original_image: np.ndarray,
     splinters: list[Splinter],
     kw_px: int,
+    n_points: int,
     z_action: Callable[[list[Splinter]], float] = None,
     clr_label: str = None,
     no_ticks: bool = True,
     plot_vertices: bool = False,
+    plot_kernel: bool = False,
     skip_edge: bool = False,
     mode: KernelContourMode = KernelContourMode.CONTOURS,
     exclude_points: list[tuple[int,int]] = None,
@@ -242,17 +245,16 @@ def plot_splinter_movavg(
         region,
         splinters,
         collector=lambda x,r: x.in_region_px(r),
-        kw_px=kw_px,
         skip_edge=skip_edge,
         skip_edge_factor=0.02,
     )
 
-    X, Y, Z = kernel.run(z_action, mode="area", exclude_points=exclude_points)
+    X, Y, Z = kernel.run(z_action,kw_px, n_points, mode="area", exclude_points=exclude_points)
 
     if normalize:
         Z = Z / np.max(Z)
 
-    return plot_kernel_results(original_image, clr_label, no_ticks, plot_vertices, mode, X, Y, Z, kw_px, figwidth=figwidth, clr_format=clr_format, crange=crange)
+    return plot_kernel_results(original_image, clr_label, no_ticks, plot_vertices, mode, X, Y, Z, kw_px, figwidth=figwidth, clr_format=clr_format, crange=crange, plot_kernel=plot_kernel)
 
 def plot_kernel_results(
     original_image,
@@ -266,7 +268,8 @@ def plot_kernel_results(
     kw_px,
     figwidth: FigWidth,
     clr_format: str = None,
-    crange: tuple[float,float] = None
+    crange: tuple[float,float] = None,
+    plot_kernel: bool = False
 ) -> StateOutput:
     """
     Plot the results of a kernel operation on an image.
@@ -296,6 +299,11 @@ def plot_kernel_results(
     def show_vertices():
         if plot_vertices:
             axs.scatter(X, Y, marker='x', c='white', s=3, linewidth=0.5)
+        if plot_kernel:
+            axs.add_patch(mpatches.Rectangle((kw_px, kw_px), kw_px, kw_px,
+                edgecolor = 'pink',
+                fill=False,
+                lw=1))
 
     if mode == KernelContourMode.CONTOURS:
         axim = axs.contourf(X, Y, results, cmap='turbo', alpha=CONTOUR_ALPHA)
@@ -305,10 +313,16 @@ def plot_kernel_results(
         show_vertices()
         show_img()
         results = cv2.resize(results, (original_image.shape[1], original_image.shape[0]), interpolation=cv2.INTER_LINEAR)
+        # results = cv2.applyColorMap(results.astype(np.uint8), cv2.COLORMAP_TURBO)
+
+        # results = cv2.cvtColor(results, cv2.COLOR_BGR2RGBA)
+        # make the outer edge of 5% of the image transparent
+        mask = make_transparent_border(results, 0.05, CONTOUR_ALPHA)
+
         if crange is None:
-            axim = axs.imshow(results, cmap='turbo', alpha=CONTOUR_ALPHA)
+            axim = axs.imshow(results, cmap='turbo', alpha=mask)
         else:
-            axim = axs.imshow(results, cmap='turbo', alpha=CONTOUR_ALPHA, vmin=crange[0], vmax=crange[1])
+            axim = axs.imshow(results, cmap='turbo', alpha=mask, vmin=crange[0], vmax=crange[1])
 
 
 