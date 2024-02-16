update functions to also analyze navids panes, fixed calculation of rhc and acceptance probability

fracsuite/core/kernels.py
@@ -1,3 +1,4 @@
+import json
 from multiprocessing import Pool
 from typing import Any, Callable, TypeVar
 import numpy as np
@@ -11,6 +12,9 @@ from rich import print
 from dataclasses import dataclass
 
 SKIP_VALUE = np.nan
+
+def deepcopy(obj):
+    return json.loads(json.dumps(obj))
 
 def convert_npoints(n_points, region, kw_px) -> tuple[int,int]:
     if isinstance(n_points, tuple):
@@ -26,21 +30,14 @@ def convert_npoints(n_points, region, kw_px) -> tuple[int,int]:
 
     return i_w, i_h
 
-def process_polar(
-        args
-    ):
-    """
-    args: r0,r1,t0,t1,i,j,ip,spl,calculator, mode, ip, pxpmm
 
-    """
-    spl: list[Splinter]
-    # radii, angles, list of splinters
-    r0,r1,t0,t1,i,j,ip,spl,calculator, mode, pxpmm = args
+#TODO: For kernels, the passed arguments are still cumbersome and not straightforward.
+#      The kerneler should be able to handle the arguments itself and raise Exceptions, if any expected argument is missing.
+#   The kerneler could start with checks:
+#       assert "n_points" in kwargs, "n_points must be set."
+#       assert "kw" in kwargs, "kw must be set."
+#       ...
 
-
-    # calculate the value
-    mean_value, stddev = calculator(spl, mode, ip, pxpmm)
-    return (i,j,(r0+r1)/2,(t0+t1)/2, mean_value, stddev)
 
 @dataclass
 class WindowArguments:
@@ -50,6 +47,7 @@ class WindowArguments:
     x2: float
     y1: float
     y2: float
+    max_d: float
     objects_in_region: list
     calculator: Callable[[list], float]
     prop: str
@@ -72,8 +70,11 @@ class WindowResult:
     stddev: float
     "Standard deviation of the region"
 
+
+
 def process_window(args: WindowArguments):
-    mean_value, stddev = args.calculator(args.objects_in_region, args.prop, args.impact_position, args.pxpmm) \
+    """Wrapper function for the window kerneler."""
+    mean_value, stddev = args.calculator(args.objects_in_region, args.prop, args.impact_position, args.pxpmm, max_distance=args.max_d, **args.kwargs) \
         if len(args.objects_in_region) > 0 else (SKIP_VALUE, SKIP_VALUE)
     return (args.i, args.j, (args.x1+args.x2)/2, (args.y1+args.y2)/2, mean_value, stddev)
 
@@ -196,6 +197,7 @@ class ObjectKerneler():
         ip_mm,
         mode,
         pxpmm,
+        **kwargs
     ):
         """
         Calculate the polar distribution of the data.
@@ -207,6 +209,7 @@ class ObjectKerneler():
             ip_mm (tuple[float,float]): Impact position in mm.
             mode (SplinterProp): Property to calculate.
             pxpmm (float): Pixel per millimeter factor.
+            kwargs: Additional arguments for the calculator.
 
         Returns:
             R,T,Z,Zstd:
@@ -245,7 +248,7 @@ class ObjectKerneler():
                     spl_groups[i][j].append(s)
 
         # create args for every (r,t) region
-        args = []
+        args: list[WindowArguments] = []
         for i in range(len(r_range_mm)-1):
             for j in range(len(t_range_deg)-1):
                 # last r includes all remaining radii
@@ -253,7 +256,10 @@ class ObjectKerneler():
                 t0,t1 = (t_range_deg[j],t_range_deg[j+1])
                 spl = spl_groups[i][j]
 
-                args.append((r0,r1,t0,t1,i,j,ip_mm,spl,calculator,mode, pxpmm))
+                c = 2*np.pi*((r1-r0)/2 + r0)
+                max_d = c * (t1-t0) / 360
+
+                args.append(WindowArguments(i,j,r0,r1,t0,t1,max_d,spl,calculator, mode, ip_mm, pxpmm, kwargs))
 
         def put_result(result):
             i, j, r_c, t_c, mean_value, stddev = result
@@ -262,15 +268,18 @@ class ObjectKerneler():
             X[i] = r_c
             Y[j] = t_c
 
+        # this might raise an error
+        process_window(args[0])
+
         if len(args) > 120:
             # use multiprocessing pool
             with Pool() as pool:
                 # create unordered imap and track progress
-                for result in tqdm(pool.imap_unordered(process_polar, args), desc='Calculating polar...', total=len(args), leave=False):
+                for result in tqdm(pool.imap_unordered(process_window, args), desc='Calculating polar...', total=len(args), leave=False):
                     put_result(result)
         else:
             for arg in tqdm(args, desc='Calculating polar...', total=len(args), leave=False):
-                result = process_polar(arg)
+                result = process_window(arg)
                 put_result(result)
 
         return X,Y,Z,Zstd
@@ -279,12 +288,26 @@ class ObjectKerneler():
         self,
         prop,
         calculator: Callable[[list[T]], float],
-        kw: int,
+        kw: float,
         n_points: int | tuple[int,int],
         impact_position: tuple[float,float],
         pxpmm: float,
         **kwargs
     ):
+        """
+        Calculate the window distribution of the data.
+
+        Args:
+            prop (_type_): _description_
+            calculator (Callable[[list[T]], float]): _description_
+            kw (int): _description_
+            n_points (int | tuple[int,int]): _description_
+            impact_position (tuple[float,float]): _description_
+            pxpmm (float): _description_
+
+        Returns:
+            X,Y,Z,Zstd
+        """
         assert n_points > 0 or n_points == -1, \
             "n_points must be greater than 0 or -1."
         assert kw < self.region[0], \
@@ -300,12 +323,6 @@ class ObjectKerneler():
         print(f'[cyan]KERNELER[/cyan] Kernel Width: {kw}')
         print(f'[cyan]KERNELER[/cyan] Points:       {n_points},{n_points} Points')
         print(f'[cyan]KERNELER[/cyan] Region:       {self.region}')
-
-        # Get the ranges for x and y
-        minx = kw // 2
-        maxx = self.region[0] - kw // 2
-        miny = kw // 2
-        maxy = self.region[1] - kw // 2
 
         i_w, i_h = convert_npoints(n_points, self.region, kw)
 
@@ -361,7 +378,11 @@ class ObjectKerneler():
 
                 objects_in_region = windows[w][h]
 
-                args.append(WindowArguments(w,h,x1,x2,y1,y2,objects_in_region,calculator,prop, impact_position, pxpmm, kwargs))
+                max_d = np.sqrt((x2-x1)**2 + (y2-y1)**2)
+                args.append(WindowArguments(w,h,x1,x2,y1,y2, max_d,objects_in_region,calculator,prop, impact_position, pxpmm, kwargs))
+
+        # make a test run, it may raise an exception
+        process_window(args[0])
 
         # iterate to calculate the values
         with Pool() as pool:

fracsuite/core/plotting.py
@@ -674,7 +674,7 @@ def datahist_to_ax(
             edgecolor = 'gray' if plot_mode == DataHistPlotMode.HIST else 'none'
 
             # density: normalize the bins data count to the total amount of data
-            binned_data,edges,bin_container = ax.hist2d(data, bins=binrange,
+            binned_data,edges,bin_container = ax.hist(data, bins=binrange,
                     density=as_density,
                     histtype='step' if plot_mode == DataHistPlotMode.STEPS else 'bar',
                     color=fillcolor,
@@ -982,7 +982,7 @@ def fill_polar_cell(
     alpha: float = 1,
 ):
     mask = np.zeros_like(img, dtype=np.uint8)
-    center = (int(center[1]), int(center[0]))
+    center = (int(center[0]), int(center[1]))
     # start by drawing a circle with the end radius
     cv2.ellipse(mask, center, (int(end_radius), int(end_radius)), 0, start_angle, end_angle, clr, -1)
 

fracsuite/core/specimen.py
@@ -102,33 +102,40 @@ def rhc_calculator(spl: list[Splinter], *args, **kwargs):
     # return mean_distance, 0
 
     total_area = np.sum([s.area for s in spl])
+    w = np.sqrt(total_area)
     d_max = 5 # default(CALC_DMAX, estimate_dmax(spl))
-    x2,y2 = lhatc_xy(all_centroids, total_area, d_max)
+    x2,y2 = lhatc_xy(all_centroids, w, w, d_max, use_weights=False)
     min_idx = first_minimum(y2)
     r1 = x2[min_idx]
 
-    # acceptance = min_idx / len(y2)
     # this is debug output
-    # fig,axs = plt.subplots(1,1)
-    # axs.plot(x2,y2)
-    # file = tempfile.mktemp(".png")
-    # fig.savefig(file)
+    if "debug" in kwargs and kwargs["debug"]:
+        fig,axs = plt.subplots(1,1)
+        axs.plot(x2,y2)
+        file = tempfile.mktemp(".png", "rhc")
+        fig.savefig(file)
+        plt.close(fig)
     return r1, 0
 
 def acc_calculator(spl: list[Splinter], *args, **kwargs):
     """Calculate the hard core radius for a given set of splinters."""
+    assert 'max_distance' in kwargs, "max_distance not passed to kernel function!"
+
     all_centroids = np.array([s.centroid_mm for s in spl])
     total_area = np.sum([s.area for s in spl])
     d_max = 5 # default(CALC_DMAX, estimate_dmax(spl))
     x2,y2 = lhatc_xy(all_centroids, total_area, d_max)
     min_idx = np.argmin(y2)
-    acceptance = min_idx / len(y2)
+
+    acceptance = min_idx / kwargs['max_distance']
 
     # this is debug output
-    # fig,axs = plt.subplots(1,1)
-    # axs.plot(x2,y2)
-    # file = tempfile.mktemp(".png")
-    # fig.savefig(file)
+    if "debug" in kwargs and kwargs["debug"]:
+        fig,axs = plt.subplots(1,1)
+        axs.plot(x2,y2)
+        file = tempfile.mktemp(".png", "acc")
+        fig.savefig(file)
+        plt.close(fig)
 
     return acceptance, 0
 
@@ -144,9 +151,6 @@ calculators = {
 class Specimen(Outputtable):
     """ Container class for a specimen. """
 
-
-    DAT_FRACINTENSITY: str = "frac_intensity"
-    "Key for the fracture intensity in the simdata file."
     DAT_HCRADIUS: str = "hc_radius"
     "Key for the hard core radius in the simdata file."
     DAT_ACCEPTANCE_PROB: str = "acceptance_prob"
@@ -322,6 +326,10 @@ class Specimen(Outputtable):
         assert self.loaded, "Specimen not loaded."
         return self.__acc
 
+    @property
+    def has_fracture_scans(self):
+        return self.__has_fracture_scans
+
     def load(self, log_missing_data: bool = False):
         """Load the specimen lazily."""
 
@@ -343,6 +351,13 @@ class Specimen(Outputtable):
         print(f"Loaded {name:>15} (Scalp: {checkmark(self.has_scalp)} , "
                 f"Splinters: {checkmark(self.has_splinters)} ) "
                     f': t={self.measured_thickness:>5.2f}mm, U={self.U:>7.2f}J/m², U_d={self.U_d:>9.2f}J/m³, σ_s={self.sig_h:>7.2f}MPa')
+
+
+    def put_fracture_image(self, img: np.ndarray):
+        """Puts the fracture image into the specimen folder."""
+        cv2.imwrite(os.path.join(self.fracture_morph_dir, "Transmission.bmp"), img)
+        self.__has_fracture_scans = True
+
 
     def __init__(self, path: str, log_missing = True, load = False):
         """Create a new specimen.
@@ -442,7 +457,7 @@ class Specimen(Outputtable):
 
         # splinters requisites
         self.fracture_morph_dir = os.path.join(self.path, "fracture", "morphology")
-        self.has_fracture_scans = os.path.exists(self.fracture_morph_dir) \
+        self.__has_fracture_scans = os.path.exists(self.fracture_morph_dir) \
             and find_file(self.fracture_morph_dir, "*.bmp") is not None
         self.splinters_path = os.path.join(self.path, "fracture", "splinter")
         "Path to the splinter output folder."
@@ -485,8 +500,20 @@ class Specimen(Outputtable):
         self.__save_simdata()
 
     def __save_simdata(self):
-        with open(self.simdata_path, "w") as f:
-            json.dump(self.simdata, f, indent=4)
+        backup = None
+        if os.path.exists(self.simdata_path):
+            with open(self.simdata_path, "r") as f:
+                backup = f.readlines()
+
+        try:
+            with open(self.simdata_path, "w") as f:
+                json.dump(self.simdata, f, indent=4)
+        except Exception as e:
+            if backup is not None:
+                with open(self.simdata_path, "w") as f:
+                    f.writelines(backup)
+
+            raise e
 
     def __save_settings(self):
         with open(self.__cfg_path, "r") as f:
@@ -520,11 +547,12 @@ class Specimen(Outputtable):
 
     def get_fracture_image(self, as_rgb = True):
         """Gets the fracture image. Default is RGB."""
-        transmission_file = find_file(self.fracture_morph_dir, "*Transmission*")
+        transmission_file = find_file(self.fracture_morph_dir, "*transmission*")
         if transmission_file is not None:
             return cv2.imread(transmission_file, cv2.IMREAD_GRAYSCALE if not as_rgb else cv2.IMREAD_COLOR)
+
     def get_transmission_image(self, as_rgb = True):
-        transmission_file = find_file(os.path.join(self.path, "anisotropy"), "*Transmission*")
+        transmission_file = find_file(os.path.join(self.path, "anisotropy"), "*transmission*")
         if transmission_file is not None:
             return cv2.imread(transmission_file, cv2.IMREAD_GRAYSCALE if not as_rgb else cv2.IMREAD_COLOR)
 
@@ -612,19 +640,24 @@ class Specimen(Outputtable):
 
         return np.array(id_list), np.array(p_list)
 
-    def calculate_NperWindow(self, force_recalc: bool = False, D_mm: float = 50) -> int:
+    def calculate_intensity(self, force_recalc: bool = False, D_mm: float = 50) -> int:
         """
-        Calculates the mean splinter count in a observation window of D_mm*D_mm on the fracture image.
+        Calculates the fracture intensity by running a kde over the whole pane domain.
+
+        The fracture intensity is
+        `lambda = N / A`
+        where N is the number of splinters and A is the area of the window. Using a KDE
+        this value is averaged over the whole pane domain.
 
         Arguments:
             force_recalc (bool, optional): Recalculate. Defaults to False.
-            D (float, optional): Window width in mm. Defaults to 50.
+            D_mm (float, optional): Window width in mm. Defaults to 50.
 
         Returns:
-            int: The mean fracture intensity, Amount of Splinters in observation field N.
+            float: The mean fracture intensity in 1/mm².
         """
 
-        f_intensity = self.simdata.get(Specimen.DAT_FRACINTENSITY, None)
+        f_intensity = self.simdata.get(Specimen.DAT_LAMBDA, None)
         if force_recalc or f_intensity is None:
             region = self.settings[Specimen.SET_REALSIZE]
             kernel = ObjectKerneler(
@@ -633,19 +666,36 @@ class Specimen(Outputtable):
                 collector=lambda x,r: x.in_region(r),
                 skip_edge=True,
             )
-
-            _, _, Z = kernel.run(
-                lambda x: len(x),
+            # run the kernel window, use int_calculator
+            _,_,Z,_ = kernel.window(
+                SplinterProp.INTENSITY,
+                int_calculator,
                 D_mm,
-                50,
-                mode="area",
-                exclude_points=[self.get_impact_position()],
-                fill_skipped_with_mean=True
+                n_points=25,
+                impact_position=self.get_impact_position(),
+                pxpmm = self.calculate_px_per_mm(),
             )
-            f_intensity = int(np.mean(Z))
-            self.set_data(Specimen.DAT_FRACINTENSITY, f_intensity)
+
+            f_intensity = np.nanmean(Z)
+            self.set_data(Specimen.DAT_LAMBDA, f_intensity)
 
         return f_intensity
+
+    def calculate_break_params(self, force_recalc: bool = False, D_mm: float = 50.0):
+        """
+        Calculate the fracture intensity parameter according to the BREAK approach.
+
+        Args:
+            force_recalc (bool, optional): Recalculate. Defaults to False.
+            D_mm (float, optional): Window width in mm. Defaults to 50.
+
+        Returns:
+            lambda, rhc, acc: The fracture intensity parameter in 1/mm², the hard core radius in mm and the acceptance probability.
+        """
+        lam = self.calculate_break_lambda(force_recalc, D_mm)
+        rhc, acc = self.calculate_break_rhc(force_recalc)
+        return lam, rhc, acc
+
 
     def calculate_break_lambda(self, force_recalc: bool = False, D_mm: float = 50.0) -> float:
         """
@@ -657,7 +707,7 @@ class Specimen(Outputtable):
         Returns:
             float: The fracture intensity parameter in 1/mm².
         """
-        lam = self.calculate_NperWindow(force_recalc=force_recalc, D_mm=D_mm) / D_mm**2
+        lam = self.calculate_intensity(force_recalc, D_mm)
         self.set_data(Specimen.DAT_LAMBDA, lam)
         return lam
 
@@ -673,23 +723,29 @@ class Specimen(Outputtable):
             d_max (float, optional): Maximum distance in mm. Defaults to 50.
 
         Returns:
-            float: The hard core radius in mm.
+            float, float: The hard core radius in mm and the acceptance probability.
         """
 
         r1 = self.simdata.get(Specimen.DAT_HCRADIUS, None)
         acceptance = self.simdata.get(Specimen.DAT_ACCEPTANCE_PROB, None)
 
         if force_recalc or r1 is None or acceptance is None:
-            all_centroids = np.array([s.centroid_mm for s in self.splinters])
+            all_centroids = np.asarray([s.centroid_mm for s in self.splinters])
             pane_size = self.get_real_size()
             d_max = estimate_dmax(self.splinters)
             x2,y2 = lhatc_xy(all_centroids, pane_size[0], pane_size[1], d_max)
             min_idx = first_minimum(y2)
-            r1 = x2[min_idx]
-            acceptance = min_idx / len(y2)
 
-            self.set_data(Specimen.DAT_ACCEPTANCE_PROB, acceptance)
-            self.set_data(Specimen.DAT_HCRADIUS, r1)
+            if min_idx == -1:
+                r1 = np.nan
+                acceptance = np.nan
+            else:
+                r1 = x2[min_idx]
+                # acceptance parameter is simply the ratio of the first minimum to the maximum distance
+                acceptance = r1 / (np.sqrt(pane_size[0]**2 + pane_size[1]**2))
+
+                self.set_data(Specimen.DAT_ACCEPTANCE_PROB, float(acceptance))
+                self.set_data(Specimen.DAT_HCRADIUS, float(r1))
 
         return r1,acceptance
 
@@ -702,10 +758,9 @@ class Specimen(Outputtable):
         if realsize_mm is not None:
             self.set_setting(Specimen.SET_REALSIZE, realsize_mm)
 
-
         frac_img = self.get_fracture_image()
         assert frac_img is not None, "Fracture image not found."
-        return frac_img.shape[0] / realsize[1]
+        return frac_img.shape[1] / realsize[0]
 
     def kfun(self):
         """
@@ -759,7 +814,24 @@ class Specimen(Outputtable):
 
         return in_region
 
-    def calculate_nfifty(self, centers = [], size = (50,50), force_recalc=False, simple = False):
+    def calculate_nfifty_kde(self, D = 50.0, force_recalc=False):
+        """
+        Calculate the nfifty value using a KDE.
+
+        The mean intensity value is calculated using `calculate_intensity`.
+        The area of the window is calculated using `D**2`. `N50 = l * D**2`.
+
+        Args:
+            D (float, optional): Window width in mm. Defaults to 50.0.
+            force_recalc (bool, optional): Forces a recalculation of the intensity, even if it has been calculated before. Defaults to False.
+
+        Returns:
+            float: The average number of splinters in a window of size D.
+        """
+        intensity = self.calculate_intensity(force_recalc, D)
+        return intensity * D**2
+
+    def calculate_nfifty_count(self, centers = [], size = (50,50), force_recalc=False, simple = False):
 
 
         if centers == [] or simple:
@@ -886,7 +958,7 @@ class Specimen(Outputtable):
             n_points,
             impact_position,
             self.calculate_px_per_mm(),
-            len(self.splinters)
+            total_len=len(self.splinters)
         )
 
         return X,Y,Z,Zstd
@@ -904,10 +976,10 @@ class Specimen(Outputtable):
             tuple[Radii(n), Angles(m), Values(n,m), Stddev(n,m)]
         """
         impact_position = self.get_impact_position()
-
+        size = self.get_real_size()
         # create kerneler
         kerneler = ObjectKerneler(
-            self.get_real_size(),
+            size,
             self.splinters,
             None,
             False

fracsuite/core/stochastics.py
@@ -370,32 +370,37 @@ def quadrat_count(points, size, d, alpha=0.05):
 
 def first_minimum(data):
     mins = argrelextrema(data, np.less, order=2)
-    first_min = mins[0][0]
+    first_min = mins[0]
+    if len(first_min) > 1:
+        first_min = first_min[0]
+    elif len(first_min) == 0:
+        first_min = -1
+
     return first_min
 
-def khat(points, width, height, d):
-    return np.array(khat_test(points, width, height, d))
+def khat(points, width, height, d, use_weights=True):
+    return np.array(khat_test(points, width, height, d, use_weights))
 
-def lhat(points, width, height, d):
-    return np.array(lhat_test(points, width, height, d))
+def lhat(points, width, height, d, use_weights=True):
+    return np.array(lhat_test(points, width, height, d, use_weights))
 
-def lhatc(points, width, height, d):
-    return np.array(lhatc_test(points, width, height, d))
+def lhatc(points, width, height, d, use_weights=True):
+    return np.array(lhatc_test(points, width, height, d, use_weights))
 
-def khat_xy(points, width, height, d):
-    p = np.array(khat_test(points, width, height, d))
+def khat_xy(points, width, height, d, use_weights=True):
+    p = khat(points, width, height, d, use_weights=use_weights)
     x = p[:,0]
     y = p[:,1]
     return x,y
 
-def lhat_xy(points, width, height, d):
-    p = np.array(lhat_test(points, width, height, d))
+def lhat_xy(points, width, height, d, use_weights=True):
+    p = lhat(points, width, height, d, use_weights=use_weights)
     x = p[:,0]
     y = p[:,1]
     return x,y
 
-def lhatc_xy(points, width, height, d):
-    p = np.array(lhatc_test(points, width, height, d))
+def lhatc_xy(points, width, height, d, use_weights=True):
+    p = lhatc(points, width, height, d, use_weights)
     x = p[:,0]
     y = p[:,1]
     return x,y

fracsuite/simulate.py
@@ -105,16 +105,11 @@ def est_break(
 ):
     specimen = Specimen.get(specimen_name)
 
-    section('Estimating intensity...')
-    # find fracture intensity from first order statistics
-    intensity = specimen.calculate_break_lambda(force_recalc=True)
-
-    section('Estimating rhc...')
-    # find the hard core radius using second order statistics
-    rhc,acceptance = specimen.calculate_break_rhc(force_recalc=True)
+    section('Estimating parameters...')
+    intensity, rhc,acceptance = specimen.calculate_break_params(force_recalc=True)
     info(f'Fracture intensity: {intensity:.2f} 1/mm²')
     info(f'HC Radius: {rhc:.2f} mm')
-    info(f'Acceptance: {acceptance:.2f}')
+    info(f'Acceptance: {acceptance:.2e}')
 
     def Kpois(d):
         # see Baddeley et al. S.206 K_pois
@@ -126,7 +121,7 @@ def est_break(
     section('Plotting kfunc...')
     x,y = specimen.kfun()
     fig, ax = plt.subplots(figsize=get_fig_width(FigureSize.ROW2))
-    ax.plot(x,y, label='$\hat{K}$')
+    ax.plot(x,y, label='$\hat{K}$') # , marker='x' # to display the points
     ax.plot(x,Kpois(np.asarray(x)), label='$\hat{K}_{t}$')
     ax.legend()
     ax.set_ylabel('$\hat{K}(d)$')
@@ -142,7 +137,7 @@ def est_break(
     fig, ax = plt.subplots(figsize=get_fig_width(FigureSize.ROW2))
     ax.plot(x2,y2, label='$\hat{L}$')
     ax.axvline(rhc, linestyle='--', color='r', label=f'$r_{{hc}}={min_L:.1f}mm$')
-    ax.plot(x2,Lpois(np.asarray(x)), label='$\hat{K}_{t}$')
+    ax.plot(x2,Lpois(np.asarray(x)), label='$\hat{L}_{t}$')
     ax.legend()
     ax.set_ylabel('$\hat{L}(d)$')
     ax.set_xlabel('$d$ (mm)')
@@ -167,7 +162,7 @@ def est_break(
     size = specimen.get_real_size()
     area = size[0] * size[1]
     n_points = int(intensity * area)
-    acceptance = 4.5e-5
+    acceptance = acceptance
     points = csstraussproc2(size[0], size[1], rhc, n_points, acceptance, int(1e6))
     fig,axs = plt.subplots()
     axs.scatter(*zip(*points))
@@ -200,7 +195,6 @@ def est_break(
 
     section('Digitalize splinters...')
     splinters = Splinter.analyze_contour_image(voronoi_img, px_per_mm=1)
-
     return splinters
 
 

fracsuite/splinters.py
@@ -119,8 +119,10 @@ def gen(
                 prep = defaultPrepConfig
 
             # generate splinters for the specimen
-            print(f'Using  px_per_mm = {px_per_mm:.2f}')
+            print("---        Summary         --")
+            print(f'       px_per_mm = {px_per_mm:.2f}')
             print(f'            prep = "{prep.name}"')
+            print(f'       real_size = {realsize}')
 
             print('Running analysis...')
             if not from_label:
@@ -333,7 +335,7 @@ def nielsen_n50(
     for i, specimen in enumerate(specimens):
         sig_s = np.abs(specimen.sig_h)
 
-        real_n50 = specimen.calculate_nfifty()
+        real_n50 = specimen.calculate_nfifty_count()
         nielsen_spec_n50 = n50(sig_s)
 
         x[i] = sig_s
@@ -379,7 +381,7 @@ def draw_contours(
     else:
         out_img = np.zeros_like(specimen.get_fracture_image(), dtype=np.uint8)
     for splinter in track(splinters, description="Drawing contours...", transient=True):
-        if color == "random":
+        if color is None:
             clr = rand_col()
         else:
             clr = norm_color(color, 255)
@@ -1676,9 +1678,9 @@ def nfifty(
     with get_progress(title='Working on specimens...') as progress:
         for i, specimen in enumerate(specimens):
             if not use_mean:
-                nfifty = specimen.calculate_nfifty(centers, (50,50), force_recalc=recalc)
+                nfifty = specimen.calculate_nfifty_count(centers, (50,50), force_recalc=recalc)
             else:
-                nfifty = specimen.calculate_NperWindow(force_recalc=recalc)
+                nfifty = specimen.calculate_intensity(force_recalc=recalc)
             results[i,:] = (specimen.U, specimen.U_d, specimen.calculate_energy(), specimen.calculate_energy_density() , specimen.thickness, bid[specimen.boundary], nfifty)
 
             progress.advance()

fracsuite/tester.py
@@ -247,7 +247,6 @@ def threshold(
     source: Annotated[str, typer.Argument(help='Path to image file.')],
     region: Annotated[tuple[int,int,int,int], typer.Option(help='')] = (250,250,100,100),
     region_f: Annotated[tuple[float,float], typer.Option(help='Region center in percent.')] = None,
-    realsize: tuple[int,int] = (500,500),
 ):
     # Initialize GUI
     root = tk.Tk()
@@ -270,7 +269,9 @@ def threshold(
         dir = os.path.dirname(source)
 
         img = specimen.get_fracture_image()
-        px_per_mm = specimen.calculate_px_per_mm(realsize_mm=realsize)
+        px_per_mm = specimen.calculate_px_per_mm()
+        print("px/mm: ", px_per_mm)
+        
         # take a small portion of the image
         if region is None:
             region = np.array((250,250,500,500)) * px_per_mm
@@ -281,9 +282,9 @@ def threshold(
             region = img.shape[1] * region_f[0], img.shape[0] * region_f[1], region[2], region[3]
             region = np.asarray(region)
 
-        print(region)
         region = region.astype(np.uint32)
         print(region)
+        print(img.shape)
         # get region cx cy w h from image
         img = img[region[1]-region[3]//2:region[1]+region[3]//2, region[0]-region[2]//2:region[0]+region[2]//2]
 